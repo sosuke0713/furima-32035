69ec54a76833dc49c07613afc72177d86610d7c5
sosuke0713 <bluecat9686@gmail.com>
2020-12-21 12:28:38 +0900

    second commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,18 +21,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(items_params)
-    if @item.save
-      redirect_to item_path
+    #@item = Item.find(params[:id])
+    if @item.valid?
+      if current_user.id == @item.user.id
+        @item.update(items_params)
+        redirect_to item_path
+      end
     else
       render :edit
     end
@@ -42,6 +48,10 @@ class ItemsController < ApplicationController
 
   def items_params
     params.require(:item).permit(:image, :title, :comments, :category_id, :status_id, :deliveryfee_id, :state_id, :shippingdate_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
